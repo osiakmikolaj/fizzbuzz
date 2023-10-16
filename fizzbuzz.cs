1c1c0ba75ffefa42075e521a758a986f06a7c6a7
Mikołaj Osiak <126515553+osiakmikolaj@users.noreply.github.com>
2023-10-16 23:22:32 +0200

    Update fizzbuzz.cs

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -1,11 +1,19 @@
-for (int i = 1; i <= 100; i++)
+using System;
+
+class Program
 {
-    if ((i % 3 == 0) && (i % 5 == 0))
-        Console.WriteLine($"{i} - FizzBuzz");
-    else if (i % 3 == 0)
-        Console.WriteLine($"{i} - Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine($"{i} - Buzz");
-    else
-        Console.WriteLine(i);
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if ((i % 3 == 0) && (i % 5 == 0))
+                Console.WriteLine($"{i} - FizzBuzz");
+            else if (i % 3 == 0)
+                Console.WriteLine($"{i} - Fizz");
+            else if (i % 5 == 0)
+                Console.WriteLine($"{i} - Buzz");
+            else
+                Console.WriteLine(i);
+        }
+    }
 }
